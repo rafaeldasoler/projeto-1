bf63b65fa7ad4b4d3b32d5426a1a4752bc9d06e8
Eliezer <eliezer13@live.com.pt>
2025-07-21 20:31:07 -0300

    Alterei o CSS para ajustar a página para mobile.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
-const slides = document.querySelector('.slides');
-const totalSlides = document.querySelectorAll('.slide').length;
+const slidesContainer = document.querySelector('.slides');
+const slideElements = document.querySelectorAll('.slide');
+const totalSlides = slideElements.length;
 let index = 0;
+
+function updateSlide() {
+  slidesContainer.style.transform = `translateX(-${index * 100}%)`;
+}
 
 document.querySelector('.btn-next').addEventListener('click', () => {
   index = (index + 1) % totalSlides;
@@ -15,3 +20,9 @@ document.querySelector('.btn-prev').addEventListener('click', () => {
 function updateSlide() {
   slides.style.transform = `translateX(-${index * 100}%)`;
 }
+
+// Fazer os slides mudarem automaticamente a cada 5 segundos
+setInterval(() => {
+  index = (index + 1) % totalSlides;
+  updateSlide();
+}, 5000);
