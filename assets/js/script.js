44269e7054604d96a5789e46e25482cb203601bd
Eliezer <eliezer13@live.com.pt>
2025-07-21 19:34:47 -0300

    Alterei o CSS para ajustar a página para mobile.

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+let index = 0;
+
+document.querySelector('.btn-next').addEventListener('click', () => {
+  index = (index + 1) % totalSlides;
+  updateSlide();
+});
+
+document.querySelector('.btn-prev').addEventListener('click', () => {
+  index = (index - 1 + totalSlides) % totalSlides;
+  updateSlide();
+});
+
+function updateSlide() {
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
